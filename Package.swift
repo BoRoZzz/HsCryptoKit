ad11604d0e6e5e6c6ae7d208b1b42c8aabd5c37a
Borys Khliebnikov <borysandco@gmail.com>
2023-02-18 21:39:22 +0000

    fix macos version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "HsCryptoKit.Swift",
     platforms: [
-        .macOS(.v10_15),
+        .macOS(.v12),
         .iOS(.v13),
     ],
     products: [
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["HsCryptoKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/horizontalsystems/HsExtensions.Swift.git", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/BoRoZzz/HsExtensions.git", branch: "main"),
         .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "3.0.0"),
         .package(url: "https://github.com/Boilertalk/secp256k1.swift.git", from: "0.1.0"),
     ],
@@ -22,7 +22,7 @@ let package = Package(
         .target(
             name: "HsCryptoKit",
             dependencies: [
-                .product(name: "HsExtensions", package: "HsExtensions.Swift"),
+                .product(name: "HsExtensions", package: "HsExtensions"),
                 .product(name: "Crypto", package: "swift-crypto"),
                 .product(name: "_CryptoExtras", package: "swift-crypto"),
                 .product(name: "secp256k1", package: "secp256k1.swift"),
